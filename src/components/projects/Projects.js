a4900f2e19da4acc79ceca48226159342f6d475c
Nivall Michael <nivahmichael@gmail.com>
2024-05-12 11:50:23 +0300

    Updated projecs part

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -17,38 +17,38 @@ const Projects = () => {
        <div className="grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 gap-6 xl:gap-14">
         <ProjectsCard
           title="Advertising Websites"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Design and develop responsive advertising web applications with interactive features, optimized 
+          for SEO, performance and analytics tracking."
           src={projectOne}
         />
         <ProjectsCard
           title="E-commerce mobile Apps"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" Create an intuitive e-commerce mobile application with seamless navigation, secure transaction
+           personalized recommendations and efficient order management."
           src={projectTwo}
         />
         <ProjectsCard
           title="E-commerce websites"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Craft dynamic e-commerce websites featuring user-friendly interfaces, secure payment gateways,
+          product catalogs and seamless checkout experience for optimal customer satisfaction."
           src={projectThree}
         />
         <ProjectsCard
           title="Portfolio websites"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des=" Develop captivating websites showcasing projects with elegant design, intuitive navigation and
+          responsive layouts for a memorable user experience."
           src={projectThree}
         />
         <ProjectsCard
           title="Management systems"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Create management website systems with intuitive interfaces, comprehensive features and secure
+           data management for streamlined organizational operations."
           src={payroll}
         />
         <ProjectsCard
           title="Inventoty management Apps"
-          des=" Lorem, ipsum dolor sit amet consectetur adipisicing elit.
-              Explicabo quibusdam voluptate sapiente voluptatibus harum quidem!"
+          des="Design inventory management apps with user-friendly interface, real-time tracking, barcode
+           scanning and reporting functionalities for efficient inventory control."
           src={projectTwo}
         />
       </div>
