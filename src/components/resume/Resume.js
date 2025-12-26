5e6839bbbd379620d9cb7423889638caca505af2
nival <nivahmichael@gmail.com>
2025-12-26 16:32:06 +0300

    updated resume page

--- src/components/resume/Resume.js
+++ src/components/resume/Resume.js
@@ -7,6 +7,7 @@ import Achievement from './Achievement';
 import Experience from './Experience';
 
 const Resume = () => {
+  
   const [educationData, setEducationData] = useState(true);
   const [skillData, setSKillData] = useState(false);
   const [experienceData, setExperienceData] = useState(false);
