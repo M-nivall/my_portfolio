63922ca41c58f0e6bff2d5ab4e2fc88d78656706
nival <nivahmichael@gmail.com>
2026-08-18 14:56:50 +0300

    Updated CV

--- src/components/banner/Banner.js
+++ src/components/banner/Banner.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import LeftBanner from './LeftBanner';
 import RightBanner from './RightBanner';
-//import cv from '../../assets/Michael_Nivah_CV.pdf';
+import cv from '../../assets/Michael_Nivah_CV.pdf';
 
 const Banner = () => {
   return (
@@ -11,14 +11,13 @@ const Banner = () => {
     >
 
       {/* ✅ DOWNLOAD CV BUTTON (TOP RIGHT OF BANNER) */}
-      {/*<a
+      <a
         href={cv}
         download="Michael_Nivah_CV.pdf"
         className="absolute top-4 right-6 z-10 px-5 py-2 bg-designColor text-black rounded-full font-semibold shadow-lg hover:scale-105 transition-all duration-300"
       >
-        /* Download CV
+        Download CV
       </a>
-        */}
 
       {/* LEFT + RIGHT CONTENT */}
       <LeftBanner />
